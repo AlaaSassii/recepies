85383a732e5746a8e9bab53871937c2662ef78ac
alaasassii <alaasassijs@gmail.com>
2022-08-14 21:59:26 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const data = [
+    {
+        id:1 , 
+        name:'molka' , 
+        personImage:'img' , 
+        email: 'example@gmail.com' , 
+        recepy : 'Pizza' , 
+        paragraph:'lorem text' , 
+
+    },
+    {
+        id:1 , 
+        name:'molka' , 
+        personImage:'img' , 
+        email: 'example@gmail.com' , 
+        recepy : 'Pizza' , 
+        paragraph:'lorem text' , 
+        
+    },
+    {
+        id:1 , 
+        name:'molka' , 
+        personImage:'img' , 
+        email: 'example@gmail.com' , 
+        recepy : 'Pizza' , 
+        paragraph:'lorem text' , 
+        
+    },
+    {
+        id:1 , 
+        name:'molka' , 
+        personImage:'img' , 
+        email: 'example@gmail.com' , 
+        recepy : 'Pizza' , 
+        paragraph:'lorem text' , 
+        
+    },
+    {
+        id:1 , 
+        name:'molka' , 
+        personImage:'img' , 
+        email: 'example@gmail.com' , 
+        recepy : 'Pizza' , 
+        paragraph:'lorem text' , 
+        
+    }
+]
